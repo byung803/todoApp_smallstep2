fb76d703fc514d4e3cb5e89647efa1c9ef73b5f2
BYUNGMIN CHOI <byung803@gmail.com>
2018-10-24 12:45:39 +0900

    add production build: source-map, script, live-server, gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'cheap-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 }
