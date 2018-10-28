fb050598f77875e415832ec9912138b6086a3a79
BYUNGMIN CHOI <byung803@gmail.com>
2018-10-28 16:14:30 +0900

    add cross-env, dotenv & use process.env for firebase key

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,13 @@
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 const config = {
-    apiKey: "AIzaSyDMLRVH92HxpfY_xAeeFw0BoszMVxiWTys",
-    authDomain: "todoapp-smallstep.firebaseapp.com",
-    databaseURL: "https://todoapp-smallstep.firebaseio.com",
-    projectId: "todoapp-smallstep",
-    storageBucket: "todoapp-smallstep.appspot.com",
-    messagingSenderId: "616869968323"
+    apiKey: process.env.FIREBASE_AUTH_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
